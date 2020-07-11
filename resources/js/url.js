ea6881aa840dfd66da241713eca0158e892f5371
jlmartinmx <jlmartin_mx@hotmail.com>
2020-07-11 00:09:47 -0500

    heroku server en urljs

--- resources/js/url.js
+++ resources/js/url.js
@@ -3,6 +3,6 @@
 //const url = 'https://walletapplicationjlmartinmx.com'
 //const url = 'http://127.0.0.1:8000'
 //const url = 'http://localhost:8000'
-const url = 'https://walletapplicationjlmartinmx.herokuapp.com/'
+const url = 'https://walletapplicationjlmartinmx.herokuapp.com'
 
 export default url
