c18e64b2a6f9a104446af5c9d2336c2edf7d6b90
jlmartinmx <jlmartin_mx@hotmail.com>
2020-07-10 23:49:51 -0500

    actualizar el url

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,7 +1,8 @@
 
 
-const url = 'https://walletapplicationjlmartinmx.com'
+//const url = 'https://walletapplicationjlmartinmx.com'
 //const url = 'http://127.0.0.1:8000'
 //const url = 'http://localhost:8000'
+const url = 'https://walletapplicationjlmartinmx.herokuapp.com/'
 
 export default url
